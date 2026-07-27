403b6be908757a9f2e82ef00b3bd01f1e1731ed6
Muskan Priyadarshi <muskanpriyadarshi9629@gmail.com>
2026-07-27 23:44:55 +0530

    Add solution for Codeforces 339A - Helpful Maths

---
+++ HelpfulMaths.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HelpfulMaths {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        char[] arr = s.replace("+", "").toCharArray();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i != arr.length - 1) {
+                System.out.print("+");
+            }
+        }
+
+        sc.close();
+    }
+}
