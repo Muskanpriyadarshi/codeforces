fd1fdb319a1c9a299d1c12227815de00a4c746b8
Muskan Priyadarshi <muskanpriyadarshi9629@gmail.com>
2026-08-02 23:53:25 +0530

    Add Codeforces 546A - Soldier and Bananas solution

---
+++ SoldierAndBananas.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SoldierAndBananas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt();
+        int n = sc.nextInt();
+        int w = sc.nextInt();
+
+        int totalCost = k * w * (w + 1) / 2;
+
+        if (totalCost > n) {
+            System.out.println(totalCost - n);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
